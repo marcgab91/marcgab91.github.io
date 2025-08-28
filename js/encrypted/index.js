d52521a631b24593e2cab11b457395c5a770c8da
marcgab91 <marcgabriel@gmx.net>
2025-08-28 13:31:31 +0200

    Update

--- js/encrypted/index.js
+++ js/encrypted/index.js
@@ -1,4 +1,3 @@
 window.protectedPages = [
   { path: 'athletes.html', title: 'athletes', encrypted: 'athletes.html' },
-  { path: 'recovery/respiration_ans_interact.html', title: 'respiration_ans_interact', encrypted: 'recovery_respiration_ans_interact.html' },
 ];
